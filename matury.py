9b88f15be15dc53097054cdaac859d17ac3b733a
Violl <s160718@student.pg.edu.pl>
2019-05-22 01:04:19 +0200

    Added function for counting average of students

--- matury.py
+++ matury.py
@@ -8,11 +8,38 @@ class Graduate:
         self.year = year = []
         self.number = number = []
 
-    def average_graduates(self, parameter_list):
-        raise NotImplementedError   
-    
-    def read_data(self, filename, new_data):
-        line_count = 0
+    def averageAttendedInYears(self, data, year, region):
+        """
+        Function calculate average number of students that attended exam in given voivodeship in years from lowest year to given year
+        ----------
+        Parameters:
+        data : Graduate
+            Object of type Graduate which contains a list for each parameter of data base
+        year : int
+            Year up to which to calculate the average
+        region : string
+            Voivodeship in which user wants to calculate the average
+        """
+        average = 0
+        for i in range(len(data.year)):
+            if 'przystąpiło' in data.attended_or_passed[i] and int(data.year[i])<=year:
+                if region in data.region[i]:
+                    average += int(data.number[i])
+        i+=1
+        return average/(1+year-int(min(data.year)))
+
+    def readData(self, filename, new_data):
+        """
+        Function reads data from .csv file and copies it into object of type Graduate
+
+        Parameters:
+        ----------
+        filename : string
+            Name of the file to be read witch filetype .csv
+        new_data : Graduate
+            Object of type Graduate which contains a list for each parameter of data base
+
+        """
         with open(filename) as csv_data:
             csv_reader = csv.reader(csv_data, delimiter=';')
             for row in csv_reader:
@@ -21,7 +48,11 @@ class Graduate:
                 new_data.sex.append(row[2])
                 new_data.year.append(row[3])
                 new_data.number.append(row[4])
-                line_count += 1
 
-new_data = Graduate('','','','','')
-new_data.read_data('baza_maturzystow.csv',new_data)
+
+data = Graduate('','','','','')
+data.readData('baza_maturzystow.csv',data)
+year = 2012
+voivodeship = 'Pomorskie'
+print(f'Średnia uczestnicząych w maturze w latach {min(data.year)}-{year}dla województwa'
+f'Pomorskiego wyniosła : {data.averageAttendedInYears(data,year,voivodeship): .1f}')
